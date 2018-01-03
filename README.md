f6e42a88b1298b440b7f271f27ea88fddbcec342
Joseph Weidinger <jsphweid@gmail.com>
2018-01-02 22:57:52 -0600

    add circle visualization

--- README.md
+++ README.md
@@ -20,3 +20,4 @@ A flip book to explore the FFTs of audio visually.
  - a way to keep previous files in the queue... If you uploaded a file and it has already processed,
     why not keep a few of the last ones in memory?. You can make an object that can represent 'this done', 'that done' and it can be a todo list
  - re-enable tests...? Why is it so hard for testing libraries to be okay with classes that have anything other than static methods in them...
+ - compress the spectrum and use colors to differentiate the magnitude
--- src/app/visualization/visualization.tsx
+++ src/app/visualization/visualization.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import { Stage, Layer, Line } from 'react-konva'
+import { transformArrayOfPointsToKonvaPointArray } from '../common/helpers'
+import { PointType } from '../common/types'
 
 export interface FlipBookProps {
     spectrum: Float32Array
@@ -8,33 +10,53 @@ export interface FlipBookProps {
 }
 
 const FlipBook: React.SFC<FlipBookProps> = (props: FlipBookProps) => {
-
-    const renderActualLine = (): JSX.Element => {
-        const distanceBetweenEachPoint: number = props.width / props.spectrum.length
-        let x: number = 0
-        const points: number[] = []
     
+    const spectrumAsNormalArr: number[] = Array.prototype.slice.call(props.spectrum)
+    const spectrumLength: number = spectrumAsNormalArr.length
+
+    const getPointsForSpectrumAsCircle = (): PointType[] => {
+        const centerPoint: PointType = { x: props.width / 2, y: props.height / 2 }
+        const angleIncrement: number = (1 / spectrumLength) * 2 * Math.PI
+        const closestWall: number = (props.width > props.height)
+            ? props.height / 2
+            : props.width / 2
+        const minimumCircleRadius: number = 0.1 * closestWall
+        const multiplier: number = 0.018 * closestWall
+        let currentAngle: number = 0
+
+        return spectrumAsNormalArr.map((value: number) => {
+            const hypotenuse: number = (value * multiplier) + minimumCircleRadius
+            const x: number = centerPoint.x + hypotenuse * Math.sin(currentAngle)
+            const y: number = centerPoint.y + hypotenuse * Math.cos(currentAngle)
+            currentAngle += angleIncrement
+            return { x, y: props.height - y }
+        })
+    }
+
+    const getPointsForSpectrumAsLine = (): PointType[] => {
+        const distanceBetweenEachPoint: number = props.width / spectrumLength
+        let x: number = 0
         const multiplier: number = props.height / 50
     
-        props.spectrum.forEach((value: number) => {
-            points.push(x)
-            points.push(props.height - (value * multiplier))
+        return spectrumAsNormalArr.map((value: number) => {
             x += distanceBetweenEachPoint
+            return { x, y: props.height - (value * multiplier) }
         })
-
-        return (
-            <Line
-                points={points}
-                stroke={'black'}
-                shadowBlur={2}
-            />
-        )
     }
+
+    const points: PointType[] = getPointsForSpectrumAsCircle() // ternary logic here
 
     return (
         <Stage width={props.width} height={props.height}>
             <Layer>
-                {props.spectrum ? renderActualLine() : null}
+                {
+                    spectrumAsNormalArr ?
+                        <Line
+                            points={transformArrayOfPointsToKonvaPointArray(points)}
+                            stroke={'black'}
+                            shadowBlur={2}
+                        /> : null
+                }
             </Layer>
         </Stage>
     )
